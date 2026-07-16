12881ba24361bf5d1ff792ac91d90e6dd343bb5a
ynulkk <18239438285@163.com>
2026-07-16 10:22:03 +0800

    Update solver.py

--- utils/solver.py
+++ utils/solver.py
@@ -1,6 +1,4 @@
-# Code for "ActionCLIP: ActionCLIP: A New Paradigm for Action Recognition"
-# arXiv:
-# Mengmeng Wang, Jiazheng Xing, Yong Liu
+
 
 import torch.optim as optim
 from utils.lr_scheduler import WarmupMultiStepLR, WarmupCosineAnnealingLR
